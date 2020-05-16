26a1c5a0b4e45c2267639667d3b9ba68f3ad2679
Yudith Rubio <yudiht17@gmail.com>
2020-05-16 14:34:33 -0700

    CafeteriaCommit

---
+++ Café/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Café.MvcApplication" Language="C#" %>
---
+++ Café/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Café.Startup))]
+namespace Café
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
