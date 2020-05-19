00bc55a1f5d482f5a779af18df1ad896da5c0a1e
Yudith Rubio <yudiht17@gmail.com>
2020-05-18 21:46:50 -0700

    MenuPostres

---
+++ Café/Controllers/PostresController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Café.Controllers
+{
+    public class PostresController : Controller
+    {
+        // GET: Postres
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
